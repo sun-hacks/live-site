1cc2e3ac192852ea0e335ada6b28a44bbe2e3f7c
Allison DiNapoli <adinapol@asu.edu>
2018-11-09 16:05:51 -0700

    updates to schedule and footer :100::sparkles:

--- src/schedule.js
+++ src/schedule.js
@@ -12,11 +12,12 @@ class Schedule extends Component {
         <h1>Schedule</h1>
         <div className="schedulebox">
           <table class="center">
-            <tr><th>Event</th><th>Location</th><th>Start Time</th><th>End Time</th></tr>
+            <tr className="scheduleheader"><th>Event</th><th>Location</th><th>Start Time</th><th>End Time</th></tr>
             {(sched.map(function(item, index) {
               var startDate = new Date(item['startTime']); 
               var endDate = new Date(item['endTime']);
-              return(<tr className="scheduleitem"><td>{item['Name']}</td><td>{item['location']}</td><td>{startDate.toLocaleString('en-US')}</td><td>{endDate.toLocaleString('en-US')}</td></tr>);
+              var testDate = new Date(2000, 1);
+              return(<tr className="schedulerow"><td>{item['Name']}</td><td>{item['location']}</td><td>{startDate.toLocaleString('en-US')}</td><td>{(endDate < testDate) ? "N/A" : endDate.toLocaleString('en-US')}</td></tr>);
             }))}
           </table>
         </div>
