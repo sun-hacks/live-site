799be25573057a79588675a3b04d602cd6a5e7f6
Cecilia La Place <laplace95@gmail.com>
2019-09-19 21:55:26 -0700

    sunhacks WR live site completion
    the evening before is now the earliest we've done it a+ us

---
+++ build/precache-manifest.15b3051a6a857bafcc24d0c8b39bccb2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b63c19480b0ab67c3bcb714c0f50e19",
+    "url": "/static/media/amazon.8b63c194.png"
+  },
+  {
+    "revision": "ff5fb53e6ac28aab84b3",
+    "url": "/static/css/main.16a9ba14.chunk.css"
+  },
+  {
+    "revision": "5dec5b25585cf493020f",
+    "url": "/static/js/1.5dec5b25.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "54b973095ce8d0e49488eb894dd9546f",
+    "url": "/static/media/sunhacks-background.54b97309.png"
+  },
+  {
+    "revision": "99626e1635959759d6981471a6f7fa55",
+    "url": "/static/media/logo-text-white.99626e16.png"
+  },
+  {
+    "revision": "daf4055650e00d5352ca3821ace7c148",
+    "url": "/static/media/agjunction.daf40556.svg"
+  },
+  {
+    "revision": "ff5fb53e6ac28aab84b3",
+    "url": "/static/js/main.ff5fb53e.chunk.js"
+  },
+  {
+    "revision": "25b452566c10d0dca65f425a7d3ae9c8",
+    "url": "/static/media/amex.25b45256.png"
+  },
+  {
+    "revision": "44eb73970adffc37796e1f9fa9f6dcd8",
+    "url": "/static/media/godaddy.44eb7397.png"
+  },
+  {
+    "revision": "b392cd7b849517acbbaba6352ec600d8",
+    "url": "/static/media/google.b392cd7b.png"
+  },
+  {
+    "revision": "6cfc759c051543b17bebcccc3b72622a",
+    "url": "/static/media/performancesoftware.6cfc759c.png"
+  },
+  {
+    "revision": "476f4b478c36be0f7563ecbca53023d7",
+    "url": "/static/media/sticker-mule.476f4b47.png"
+  },
+  {
+    "revision": "bfa2c227c93518299ec2e85e60e18081",
+    "url": "/index.html"
+  }
+];
---
+++ js/scripts.js
@@ -0,0 +1,307 @@
+schedule = {"Friday": 
+		[
+	  {
+	    "Start": "5:00PM",
+	    "End": "7:30PM",
+	    "Description": "Doors open for participants",
+	    "Location": "Normal Ave"
+	  },
+	  {
+	    "Start": "7:00PM",
+	    "End": "8:00PM",
+	    "Description": "Waitlist Checkin Opens",
+	    "Location": "Lobby"
+	  },
+	  {
+	    "Start": "7:15PM",
+	    "End": "8:00PM",
+	    "Description": "Opening Ceremonies",
+	    "Location": "Gold Gym"
+	  },
+	  {
+	    "Start": "8:00PM",
+	    "End": "",
+	    "Description": "Dinner",
+	    "Location": "Maroon Gym"
+	  },
+	  {
+	    "Start": "8:00PM",
+	    "End": "8:30PM",
+	    "Description": "Team Building Event",
+	    "Location": "Lobby"
+	  },
+	  {
+	    "Start": "8:30PM",
+	    "End": "",
+	    "Description": "Hacking Begins",
+	    "Location": "Maroon Gym"
+	  },
+	  {
+	    "Start": "8:00PM",
+	    "End": "11:59PM",
+	    "Description": "Hardware Lab",
+	    "Location": "Discovery Center"
+	  },
+	  {
+	    "Start": "9:00PM",
+	    "End": "9:30PM",
+	    "Description": "Hacking Your First Hackathon",
+	    "Location": "Gold Gym"
+	  },
+	  {
+	    "Start": "10:00PM",
+	    "End": "10:30PM",
+	    "Description": "Git Familiar with Version Control",
+	    "Location": "Gold Gym"
+	  },
+	  {
+	    "Start": "10:45PM",
+	    "End": "2:00AM",
+	    "Description": "Shower Time",
+	    "Location": "Locker Room"
+	  }
+	],
+			"Saturday": [
+  {
+    "Start": "12:00AM",
+    "End": "12:00PM",
+    "Description": "Napathon",
+    "Location": "Well Devil Suite"
+  },
+  {
+    "Start": "12:00AM",
+    "End": "11:59PM",
+    "Description": "Hardware Lab\tDiscovery Center",
+    "Location": ""
+  },
+  {
+    "Start": "12:00AM",
+    "End": "12:45AM",
+    "Description": "Paper Airplane Contest\tLobby",
+    "Location": ""
+  },
+  {
+    "Start": "1:00AM",
+    "End": "2:00AM",
+    "Description": "Zero to Hero",
+    "Location": "Gold Gym"
+  },
+  {
+    "Start": "2:30AM",
+    "End": "3:30AM",
+    "Description": "Zero to Hero II",
+    "Location": "Gold Gym"
+  },
+  {
+    "Start": "8:00AM",
+    "End": "",
+    "Description": "Breakfast",
+    "Location": "Gold Gym"
+  },
+  {
+    "Start": "8:00AM",
+    "End": "9:00AM",
+    "Description": "Machine Learning with American Express",
+    "Location": ""
+  },
+  {
+    "Start": "9:30AM",
+    "End": "10:30AM",
+    "Description": "Intro to React + DNS with GoDaddy",
+    "Location": "Gold Gym"
+  },
+  {
+    "Start": "11:00AM",
+    "End": "12:00PM",
+    "Description": "REST APIs with State Farm",
+    "Location": "Gold Gym"
+  },
+  {
+    "Start": "12:00PM",
+    "End": "",
+    "Description": "Lunch",
+    "Location": "Gold Gym"
+  },
+  {
+    "Start": "1:00PM",
+    "End": "2:00PM",
+    "Description": "sunhacks.add(you)",
+    "Location": "Gold Gym"
+  },
+  {
+    "Start": "2:30PM",
+    "End": "3:30PM",
+    "Description": "But why AR/VR?",
+    "Location": "Gold Gym"
+  },
+  {
+    "Start": "3:30PM",
+    "End": "4:30PM",
+    "Description": "WiCS Coffee Hour",
+    "Location": "Well Devil Suite"
+  },
+  {
+    "Start": "5:00PM",
+    "End": "6:00PM",
+    "Description": "The Importance of Soft Skills & Interdisciplinary Tech",
+    "Location": "Gold Gym"
+  },
+  {
+    "Start": "7:00PM",
+    "End": "",
+    "Description": "Dinner",
+    "Location": "Gold Gym"
+  },
+  {
+    "Start": "8:00PM",
+    "End": "9:00PM",
+    "Description": "Deep Learning",
+    "Location": "Gold Gym"
+  },
+  {
+    "Start": "9:30PM",
+    "End": "10:30PM",
+    "Description": "Slideshow Karaoke",
+    "Location": ""
+  },
+  {
+    "Start": "11:00PM",
+    "End": "2:45AM",
+    "Description": "Shower Time",
+    "Location": "Locker Room"
+  }
+],
+			"Sunday": [
+  {
+    "Start": "12:00AM",
+    "End": "7:00AM",
+    "Description": "Napathon",
+    "Location": "Well Devil Suite"
+  },
+  {
+    "Start": "12:00AM",
+    "End": "12:30AM",
+    "Description": "Putting Your Dev' in Devpost",
+    "Location": "Gold Gym"
+  },
+  {
+    "Start": "1:00AM",
+    "End": "2:00AM",
+    "Description": "JS/MS Paint with Bob Ross",
+    "Location": "Gold Gym"
+  },
+  {
+    "Start": "7:00AM",
+    "End": "",
+    "Description": "Hardware Lab Closes",
+    "Location": "Discovery Center"
+  },
+  {
+    "Start": "7:30AM",
+    "End": "",
+    "Description": "Devposts Submissions due",
+    "Location": "Maroon Gym"
+  },
+  {
+    "Start": "8:00AM",
+    "End": "",
+    "Description": "Breakfast",
+    "Location": "Gold Gym"
+  },
+  {
+    "Start": "8:00AM",
+    "End": "",
+    "Description": "Hacking End.",
+    "Location": "Maroon Gym"
+  },
+  {
+    "Start": "8:00AM",
+    "End": "9:00AM",
+    "Description": "Set up for demos",
+    "Location": "Maroon Gym"
+  },
+  {
+    "Start": "9:00AM",
+    "End": "11:00AM",
+    "Description": "Judging",
+    "Location": "Maroon Gym"
+  },
+  {
+    "Start": "11:00AM",
+    "End": "11:30AM",
+    "Description": "Return items to Hardware Lab",
+    "Location": "Discovery Center"
+  },
+  {
+    "Start": "11:30AM",
+    "End": "12:15PM",
+    "Description": "Closing Ceremonies",
+    "Location": "Gold Gym"
+  },
+  {
+    "Start": "1:30PM",
+    "End": "",
+    "Description": "Hardware Lab items due.",
+    "Location": ""
+  }
+]}
+
+var divContainer = document.getElementById("scheduleTable");
+divContainer.innerHTML = "";
+var col = [];
+for (var i = 0; i < schedule["Friday"].length; i++) {
+    for (var key in schedule["Friday"][i]) {
+        if (col.indexOf(key) === -1) {
+            col.push(key);
+        }
+    }
+}
+
+var table = document.createElement("table");
+var tr = table.insertRow(-1);
+var th = document.createElement("th");
+th.innerHTML = "Day";
+tr.appendChild(th);
+for (var i = 0; i < col.length; i++) {
+    var th = document.createElement("th");
+    th.innerHTML = col[i];
+    tr.appendChild(th);
+}
+for(day in schedule){
+	for (var i = 0; i < schedule[day].length; i++) {
+
+	    tr = table.insertRow(-1);
+		var tabCell = tr.insertCell(-1);
+		tabCell.innerHTML = day;
+	    for (var j = 0; j < col.length; j++) {
+	        tabCell = tr.insertCell(-1);
+	        tabCell.innerHTML = schedule[day][i][col[j]];
+	    }
+	}
+
+	divContainer.appendChild(table);
+}
+
+document.getElementById("defaultOpen").click();
+
+
+function switchTabs(evt, tabName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
